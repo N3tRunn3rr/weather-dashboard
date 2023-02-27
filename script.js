3c6f816c926fe1f01e7f2fbc4309c497f1da2e62
Kenji Fleming <kwf1999@gmail.com>
2023-02-27 15:28:26 -0500

    added code to HTML to reference in JS in order to display weather info from API fethces

--- script.js
+++ script.js
@@ -41,9 +41,28 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=$
   .then(function(response){
     console.log(response)
     
-    var forecastTime = response.list[3].dt_txt;
-    var timeArray = forecastTime.split(' ');
+    var forecastArray1 = response.list[3].dt_txt;
+    var timeArray = forecastArray1.split(' ');
     console.log(timeArray);
+
+    var forecastArray2 = response.list[11].dt_txt;
+    var timeArray2 = forecastArray2.split(' ');
+    console.log(timeArray2);
+
+    var forecastArray3 = response.list[19].dt_txt;
+    var timeArray3 = forecastArray3.split(' ');
+    console.log(timeArray3);
+
+    var forecastArray4 = response.list[27].dt_txt;
+    var timeArray4 = forecastArray4.split(' ');
+    console.log(timeArray4);
+
+    var forecastArray5 = response.list[5].dt_txt;
+    var timeArray5 = forecastArray5.split(' ');
+    console.log(timeArray5);
+
+    //TODO: work on getting the results back for the same time block each day
+    //TODO: possibly using an if else statement with between two values to select only those specific time blocks that I want the datasets for
   })
   });
 };
